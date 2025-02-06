a6ccb0842af0795d72b9b5ac9603815c2299b585
Sebastian Rodriguez <rdzsebastian@gmail.com>
2025-02-06 12:21:09 -0300

    Cambios en video

--- Scripts/level.gd
+++ Scripts/level.gd
@@ -20,15 +20,9 @@ extends Node3D
 
 @onready var rich_text_label = $CanvasLayer/RichTextLabel
 
-var fade_duration = 1.0
-var fade_tween: Tween
-var ready_to_fade_out: bool = false
-
 func _ready():
 	gui.start()
 	rich_text_label.modulate = Color.TRANSPARENT
-	fade_in()
-	await get_tree().create_timer(fade_duration).timeout
 
 func perdiste():
 	camera.camera_shake()
@@ -47,7 +41,6 @@ func detener_enemigos():
 			hijo.stop()
 
 func _on_timer_timeout() -> void:
-	ready_to_fade_out = true
 	
 	stop_spawners()
 	
@@ -63,21 +56,7 @@ func delete_enemies():
 
 func _physics_process(_delta: float) -> void:
 	gui.tiempo(timer.time_left + 1)
-	if ready_to_fade_out:
-		fade_out()
-		await get_tree().create_timer(fade_duration).timeout
 
-func fade_in():
-	if fade_tween: fade_tween.kill()
-	fade_tween = get_tree().create_tween()
-	fade_tween.tween_property(rich_text_label, "modulate", Color.WHITE, fade_duration)
-	fade_tween.finished.connect(func(): ready_to_fade_out = true)
-
-func fade_out():
-	if fade_tween: fade_tween.kill()
-	fade_tween = get_tree().create_tween()
-	fade_tween.tween_property(rich_text_label, "modulate", Color.TRANSPARENT, fade_duration)
-	fade_tween.finished.connect(func(): ready_to_fade_out = false)
 
 func stop_spawners():
 	var sceneName = get_tree().current_scene.name
